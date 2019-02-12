1eec5f7c050542e946fef7289bc25e82ef949550
A Node.js dev <john.doe@example.com>
2019-02-12 13:22:20 +0100

    implement resolveObjects.js

---
+++ part 1/resolveObjects.js
@@ -0,0 +1,24 @@
+module.exports = function resolveObjects(obj) {
+  let objCopy = { ...obj };
+  for (let key of Object.keys(objCopy)) {
+    // Recursively resolve properties of type object
+    if (typeof objCopy[key] === "object" && !Array.isArray(objCopy[key])) {
+      resolveObjects(objCopy[key]);
+    } else if (key.includes(".")) {
+      let queue = key.split(".");
+      let value = objCopy[key];
+      let currentObj = objCopy;
+      // Create/Alter properties from queue
+      for (let i = 0; i < queue.length; i++) {
+        // If property doesn't exist; Create it!
+        if (!currentObj[queue[i]]) {
+          currentObj[queue[i]] = i === queue.length - 1 ? value : {};
+        }
+        currentObj = currentObj[queue[i]];
+      }
+      // Clean up
+      delete objCopy[key];
+    }
+  }
+  return objCopy;
+};
